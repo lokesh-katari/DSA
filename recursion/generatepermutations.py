a14ebd2a917c0c979316d3370c127b01abd9821b
lokesh-katari <lokeshkatari921@outlook.com>
2024-12-05 13:26:30 +0530

    generatevalidparenthesis
    Signed-off-by: lokesh-katari <lokeshkatari921@outlook.com>

--- recursion/generatepermutations.py
+++ recursion/generatepermutations.py
@@ -10,8 +10,24 @@ def genp(arr,temparr):
            genp(arr,temparr)
            temparr.pop()
            boolarr[i] = False
+def genpString(str,tempstr):
+    if(len(str)== len(tempstr)):
+        print(tempstr)
+        return
+    for i in range(len(str)):
+        if(not boolarr2[i]):
+            boolarr2[i] = True
+            tempstr+=str[i]
+            genpString(str,tempstr)
+            tempstr.pop()
+            boolarr2[i]=False
+
+
 arr = [1,2,3]
 ind = 0
 boolarr =[False]*len(arr)
+samstr = "lokesh"
+boolarr2 =[False]*len(samstr)
 print(boolarr)
 genp(arr,[])
+genpString(samstr,"")
